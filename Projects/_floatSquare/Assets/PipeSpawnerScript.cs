ddc1c3ee973efc6fbd131a15328f9f8431181335
Maria B <143831892+marb543@users.noreply.github.com>
2025-01-30 18:12:16 -0500

    feat : Fix Pipe Generation

--- Projects/_floatSquare/Assets/PipeSpawnerScript.cs
+++ Projects/_floatSquare/Assets/PipeSpawnerScript.cs
@@ -1,14 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
-public class PipeSpawnerScript : MonoBehaviour
+public class PipeSpawnScript : MonoBehaviour
 {
     public GameObject pipe;
-    public float spawnRate = 2f;
-    private float timer = 0f;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public float spawnRate = 2;
+    private float timer = 0;
+    public float heightOffset = 10;
+
+    // Start is called before the first frame update
     void Start()
     {
-
+        spawnPipe();
     }
 
     // Update is called once per frame
@@ -16,12 +20,21 @@ public class PipeSpawnerScript : MonoBehaviour
     {
         if (timer < spawnRate)
         {
-            timer += Time.deltaTime; // Fixed incorrect usage of deltaTime
+            timer = timer + Time.deltaTime;
         }
         else
         {
-            Instantiate(pipe, transform.position, transform.rotation);
-            timer = 0f; // Reset the timer after spawning
+            spawnPipe();
+            timer = 0;
         }
+
+    }
+
+    void spawnPipe()
+    {
+        float lowestPoint = transform.position.y - heightOffset;
+        float highestPoint = transform.position.y + heightOffset;
+
+        Instantiate(pipe, new Vector3(transform.position.x, Random.Range(lowestPoint, highestPoint), 0), transform.rotation);
     }
 }
