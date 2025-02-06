af2cc8933fa2085aab6f0658fdc37160b41797e1
Maria B <143831892+marb543@users.noreply.github.com>
2025-02-06 13:53:08 -0500

    dev : Add logic for collision on pipes

---
+++ Projects/_floatSquare/Assets/LogicScript.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LogicScript : MonoBehaviour
+{
+    public int playerScore;
+    public Text scoreText;
+    public GameObject gameOverScreen;
+
+    [ContextMenu("Increase Score")]
+    public void addScore(int scoreToAdd)
+    {
+        playerScore = playerScore + scoreToAdd;
+        scoreText.text = playerScore.ToString();
+    }
+
+
+}
--- Projects/_floatSquare/Assets/PipeMoveScript.cs
+++ Projects/_floatSquare/Assets/PipeMoveScript.cs
@@ -1,9 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class PipeMoveScript : MonoBehaviour
 {
     public float moveSpeed = 5;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public float deadZone = -45;
+
+    // Start is called before the first frame update
     void Start()
     {
 
@@ -12,6 +16,9 @@ public class PipeMoveScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position += Vector3.left * moveSpeed * Time.deltaTime;
+        if (transform.position.x > deadZone)
+        {
+            Destroy(gameObject);
+        }
     }
 }
